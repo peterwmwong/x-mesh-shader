a654752c1427929039f703bc8604b7063d1dc677
Peter Wong <peter.wm.wong@gmail.com>
2022-06-10 15:10:32 -0500

    Attempt simple mesh shader with a single object/primtiive/vertex (point).

--- x-mesh-shader/Renderer.swift
+++ x-mesh-shader/Renderer.swift
@@ -11,19 +11,26 @@ struct Renderer {
        self.commandQueue = device.makeCommandQueue()!
         
         let lib = device.makeDefaultLibrary()!
-        let desc = MTLRenderPipelineDescriptor()
-        desc.vertexFunction = lib.makeFunction(name: "vert_main")
+        let desc = MTLMeshRenderPipelineDescriptor()
+        
+        desc.objectFunction = lib.makeFunction(name: "obj_main")
+        desc.maxTotalThreadsPerObjectThreadgroup = 16;
+        
+        desc.meshFunction = lib.makeFunction(name: "mesh_main")
+        desc.maxTotalThreadsPerMeshThreadgroup = 16;
+        
         desc.fragmentFunction = lib.makeFunction(name: "frag_main")
+        
         desc.colorAttachments[0]?.pixelFormat = .bgra8Unorm
 
-        renderPipeline = try device.makeRenderPipelineState(descriptor: desc)
+        (renderPipeline, _) = try device.makeRenderPipelineState(descriptor: desc, options: MTLPipelineOption())
     }
     
     public func encodeRender(target: MTLTexture, desc: MTLRenderPassDescriptor) -> MTLCommandBuffer {
         let commandBuffer = commandQueue.makeCommandBuffer()!
         let enc = commandBuffer.makeRenderCommandEncoder(descriptor: desc)!
         enc.setRenderPipelineState(renderPipeline)
-        enc.drawPrimitives(type: .point, vertexStart: 0, vertexCount: 1)
+        enc.drawMeshThreads(MTLSizeMake(1, 1, 1), threadsPerObjectThreadgroup: MTLSizeMake(1, 1, 1), threadsPerMeshThreadgroup: MTLSizeMake(1, 1, 1))
         enc.endEncoding()
         return commandBuffer
     }
--- x-mesh-shader/Shaders.metal
+++ x-mesh-shader/Shaders.metal
@@ -1,17 +1,31 @@
 #include <metal_stdlib>
 using namespace metal;
 
+[[object]]
+void obj_main(mesh_grid_properties mgp) {
+    mgp.set_threadgroups_per_grid(uint3(1, 1, 1));
+}
+
 struct VertexOut {
     float4 position   [[position]];
     float  point_size [[point_size]];
 };
 
-[[vertex]]
-VertexOut vert_main() {
-    return {
-        .position = float4(0, 0, 0, 1),
+using Mesh = metal::mesh<
+    VertexOut, // Vertex Type
+    void,      // Primitive Type
+    16,        // Max Vertices
+    16,        // Max Primitives
+    metal::topology::point
+>;
+
+[[mesh]]
+void mesh_main(Mesh m) {
+    m.set_vertex(0, {
+        .position = float4(0.0, 0.0, 0.0, 1),
         .point_size = 512.0
-    };
+    });
+    m.set_primitive_count(1);
 }
 
 [[fragment]]
