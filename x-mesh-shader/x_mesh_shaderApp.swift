976905bed1fd64a3e490319d0b4b7afa2cd20b1b
Peter Wong <peter.wm.wong@gmail.com>
2022-06-10 14:01:03 -0500

    Initial Commit

---
+++ x-mesh-shader/x_mesh_shaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  x_mesh_shaderApp.swift
+//  x-mesh-shader
+//
+//  Created by Peter Wong on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct x_mesh_shaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
