c23c389a480efce1f75fe8df8f8c2acbe160ce96
Peter Wong <peter.wm.wong@gmail.com>
2022-06-11 15:21:37 -0500

    Working! Render 2 points!
    IMPORTANT: Only one primitive can be created from a thread group.

--- x-mesh-shader/Renderer.swift
+++ x-mesh-shader/Renderer.swift
@@ -1,36 +1,48 @@
 import SwiftUI
 import Metal
 
+let DEBUG_BUFFER_BYTE_SIZE = 128
+
+// TODO: START HERE
+// TODO: START HERE
+// TODO: START HERE
+// Render triangles
+
 struct Renderer {
     let device: MTLDevice
     let commandQueue: MTLCommandQueue
     let renderPipeline: MTLRenderPipelineState
     
+    // Debugging only
+    let debug_obj_buffer: MTLBuffer
+    let debug_mesh_buffer: MTLBuffer
+    
     public init(device: MTLDevice) throws {
-       self.device = device
-       self.commandQueue = device.makeCommandQueue()!
+        self.device = device
+        self.commandQueue = device.makeCommandQueue()!
+        self.debug_obj_buffer = device.makeBuffer(length: DEBUG_BUFFER_BYTE_SIZE, options: [.storageModeShared])!;
+        self.debug_mesh_buffer = device.makeBuffer(length: DEBUG_BUFFER_BYTE_SIZE, options: [.storageModeShared])!;
         
         let lib = device.makeDefaultLibrary()!
         let desc = MTLMeshRenderPipelineDescriptor()
-        
         desc.objectFunction = lib.makeFunction(name: "obj_main")
-        desc.maxTotalThreadsPerObjectThreadgroup = 16;
-        
         desc.meshFunction = lib.makeFunction(name: "mesh_main")
-        desc.maxTotalThreadsPerMeshThreadgroup = 16;
-        
         desc.fragmentFunction = lib.makeFunction(name: "frag_main")
-        
         desc.colorAttachments[0]?.pixelFormat = .bgra8Unorm
-
-        (renderPipeline, _) = try device.makeRenderPipelineState(descriptor: desc, options: MTLPipelineOption())
+        
+        (self.renderPipeline, _) = try device.makeRenderPipelineState(descriptor: desc, options: MTLPipelineOption())
     }
     
     public func encodeRender(target: MTLTexture, desc: MTLRenderPassDescriptor) -> MTLCommandBuffer {
         let commandBuffer = commandQueue.makeCommandBuffer()!
         let enc = commandBuffer.makeRenderCommandEncoder(descriptor: desc)!
         enc.setRenderPipelineState(renderPipeline)
-        enc.drawMeshThreads(MTLSizeMake(1, 1, 1), threadsPerObjectThreadgroup: MTLSizeMake(1, 1, 1), threadsPerMeshThreadgroup: MTLSizeMake(1, 1, 1))
+        enc.setObjectBuffer(debug_obj_buffer, offset: 0, index: 0)
+        enc.setMeshBuffer(debug_mesh_buffer, offset: 0, index: 0)
+        enc.drawMeshThreads(MTLSizeMake(2, 1, 1), // grid size
+                            threadsPerObjectThreadgroup: MTLSizeMake(1, 1, 1),
+                            threadsPerMeshThreadgroup: MTLSizeMake(1, 1, 1)
+        )
         enc.endEncoding()
         return commandBuffer
     }
--- x-mesh-shader/Shaders.metal
+++ x-mesh-shader/Shaders.metal
@@ -1,34 +1,57 @@
 #include <metal_stdlib>
 using namespace metal;
 
-[[object]]
-void obj_main(mesh_grid_properties mgp) {
-    mgp.set_threadgroups_per_grid(uint3(1, 1, 1));
+[[object, max_total_threadgroups_per_mesh_grid(1), max_total_threads_per_threadgroup(1)]]
+void obj_main(       mesh_grid_properties mgp,
+                     uint                 tid [[thread_position_in_grid]],
+              device uchar*               debug_obj_buffer) {
+    debug_obj_buffer[tid] = 128;
+    mgp.set_threadgroups_per_grid(uint3(2, 1, 1));
 }
 
-struct VertexOut {
+struct VertexData {
     float4 position   [[position]];
     float  point_size [[point_size]];
 };
 
+struct PrimitiveData {
+    float3 color;
+};
+
 using Mesh = metal::mesh<
-    VertexOut, // Vertex Type
-    void,      // Primitive Type
-    16,        // Max Vertices
-    16,        // Max Primitives
+    VertexData,    // Vertex Type
+    PrimitiveData, // Primitive Type
+    1,             // Max Vertices
+    1,             // Max Primitives
     metal::topology::point
 >;
 
-[[mesh]]
-void mesh_main(Mesh m) {
-    m.set_vertex(0, {
-        .position = float4(0.0, 0.0, 0.0, 1),
-        .point_size = 512.0
-    });
-    m.set_primitive_count(1);
+[[mesh, max_total_threads_per_threadgroup(1)]]
+void mesh_main(       Mesh           m,
+                      uint           tid_in_grid  [[thread_position_in_grid]],
+                      uint           tid_in_group [[thread_position_in_threadgroup]],
+                      uint           gid_in_grid  [[threadgroup_position_in_grid]],
+               device packed_uchar3* debug_mesh_buffer) {
+    debug_mesh_buffer[tid_in_grid] = uchar3(tid_in_grid, tid_in_group, gid_in_grid) + 1;
+    if (tid_in_group == 0) {
+        // Set only once per primitive
+        m.set_primitive_count(1);
+        m.set_primitive(tid_in_group, { .color = float3(1., 0., 0.) });
+        
+        // TODO: What does set_index do?
+        // m.set_index(lid, lid);
+        m.set_vertex(tid_in_group, {
+            .position = float4(float(gid_in_grid) * 0.5, 0.0, 0.0, 1),
+            .point_size = 128.0
+        });
+    }
 }
 
+struct FragmentIn {
+    PrimitiveData primitive;
+};
+
 [[fragment]]
-half4 frag_main() {
-    return half4(0, 1, 0, 1);
+half4 frag_main(FragmentIn in [[stage_in]]) {
+    return half4(half3(in.primitive.color), 1);
 }
